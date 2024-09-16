fb46de0921485831812a902eba25b9b03fb19595
Joachim Neu <email@joachim-neu.de>
2024-09-16 11:37:15 -0400

    Enable user config with hook for entry processing pre-rendering

--- regenbib/cli_render.py
+++ regenbib/cli_render.py
@@ -1,8 +1,39 @@
 #! /usr/bin/env python3
 
+import sys
+import os
+import copy
 import argparse
+import importlib.util
 import bibtex_dblp.database
 from .store import Store
+
+
+def load_cfgpy(cfgpy_filename):
+    if not os.path.exists(cfgpy_filename):
+        return {}
+
+    original_dont_write_bytecode = sys.dont_write_bytecode
+    sys.dont_write_bytecode = True
+
+    cfgpy_spec = importlib.util.spec_from_file_location('cfgpy', cfgpy_filename)
+    cfgpy = importlib.util.module_from_spec(cfgpy_spec)
+    cfgpy_spec.loader.exec_module(cfgpy)
+
+    sys.dont_write_bytecode = original_dont_write_bytecode
+
+    return cfgpy
+
+
+def default_render_entry_hook(entry, entry_pybtex):
+    if entry_pybtex.fields.get('series', '') == 'Lecture Notes in Computer Science':
+        entry_pybtex.fields['series'] = 'LNCS'
+
+    if entry_pybtex.fields.get('url', '').startswith('https://eprint.iacr.org/'):
+        entry_pybtex.fields['note'] = entry_pybtex.fields.get('note', '')
+        del entry_pybtex.fields['note']
+
+    return (entry, entry_pybtex)
 
 
 def run():
@@ -12,22 +43,20 @@ def run():
                         default='references.yaml', help='File name of .yaml file')
     parser.add_argument('--bib', metavar='BIB_FILE', type=str,
                         default='references.bib', help='File name of .bib file')
+    parser.add_argument('--cfgpy', metavar='CFGPY_FILE', type=str,
+                        default='regenbib.cfg.py', help='File name of .cfg.py file')
     args = parser.parse_args()
 
     store = Store.load_or_empty(args.yaml)
     bib = bibtex_dblp.database.parse_bibtex('')
+    cfgpy = load_cfgpy(args.cfgpy)
+
+    render_entry_hook = cfgpy.get('render_entry_hook', default_render_entry_hook)
 
     for entry in store.entries:
         print(entry)
         entry_pybtex = entry.render_pybtex_entry()
-
-        if entry_pybtex.fields.get('series', '') == 'Lecture Notes in Computer Science':
-            entry_pybtex.fields['series'] = 'LNCS'
-
-        if entry_pybtex.fields.get('url', '').startswith('https://eprint.iacr.org/'):
-            entry_pybtex.fields['note'] = entry_pybtex.fields.get('note', '')
-            del entry_pybtex.fields['note']
-
+        (entry, entry_pybtex) = render_entry_hook(copy.deepcopy(entry), copy.deepcopy(entry_pybtex))
         print(entry_pybtex)
 
         bib.entries[entry.bibtexid] = entry_pybtex
